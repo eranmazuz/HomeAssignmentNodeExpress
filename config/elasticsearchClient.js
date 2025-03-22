48ae0e6065399a89dde0a41cac57a2bd2e0ff423
eranmazuz <mazuzeran@gmail.com>
2025-03-22 23:38:42 +0200

    - Create express server - set port from environment variable.

--- config/elasticsearchClient.js
+++ config/elasticsearchClient.js
@@ -1,7 +1,7 @@
 const { Client } = require('@elastic/elasticsearch');
 
 const client = new Client({
-    node: process.env.ELASTICSEARCH_NODE, // Replace with your Elasticsearch instance URL
+    node: process.env.ELASTICSEARCH_NODE
 });
 
 module.exports = client;
